f83b09ec201d51d66041e2792d92d7f14365b142
Dariusz Gertych <dariusz.gertych@gmail.com>
2011-03-15 22:54:17 +0100

    get as much information as possible, fix fetchining of 'uid'

--- lib/oa-blip.rb
+++ lib/oa-blip.rb
@@ -1,4 +1,5 @@
 require 'omniauth/oauth'
+require 'multi_json'
 
 module OmniAuth
   module Strategies
@@ -9,12 +10,33 @@ module OmniAuth
       #
       #    use OmniAuth::Strategies::Blip, 'consumerkey', 'consumersecret'
       #
-      def initialize(app, consumer_key, consumer_secret)
-        super(app, :blip, consumer_key, consumer_secret, :site => 'http://blip.pl')
+      def initialize(app, consumer_key, consumer_secret, options = {}, &block)
+        options.merge!({:site => 'http://blip.pl'})
+        super(app, :blip, consumer_key, consumer_secret, options)
       end
 
       def auth_hash
-        OmniAuth::Utils.deep_merge(super, 'uid' => @access_token.params[:user_id])
+        OmniAuth::Utils.deep_merge(super, {
+          'uid'       => user_hash['id'].to_s,
+          'user_info' => user_info,
+          'extra'     => {'user_hash' => user_hash}
+        })
+      end
+
+      def user_info
+        {
+          'nickname'  => user_hash['login'],
+          'location'  => user_hash['location'],
+          'image'     => image_url
+        }
+      end
+
+      def user_hash
+        @user_hash ||= MultiJson.decode @access_token.get('/profile').body
+      end
+
+      def image_url
+        @image_url ||= MultiJson.decode(@access_token.get(user_hash['avatar_path']).body)['url'] rescue nil
       end
 
     end
